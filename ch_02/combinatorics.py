603812f2203cb8d64dd8b8331188b5ce75c1ba18
Tom Zhang <tommyawesome08@gmail.com>
2025-08-05 14:25:48 -0400

    CH2 - Combinatorics

--- ch_02/combinatorics.py
+++ ch_02/combinatorics.py
@@ -1,11 +1,14 @@
-# brute force
-# O(n^3)
+# reduce time complexity from O(n^3) to O(n^2)
+
+import math
+
 def solve_quadratic():
     for a in range(1, 100):
         for b in range(1, 100):
-            for c in range(1, 100):
-                if a**2 + b**2 == c**2:
-                    print(f"A: {a}, B: {b} C: {c}")
+            c = int(math.sqrt(a * a + b * b))
+
+            if a**2 + b**2 == c**2:
+                print(f"A: {a}, B: {b}, C: {c}")
 
 
 solve_quadratic()
