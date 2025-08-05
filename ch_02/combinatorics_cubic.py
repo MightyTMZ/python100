5f2ba4e7180f5af6ecb3c2e767db31172629a30e
Tom Zhang <tommyawesome08@gmail.com>
2025-08-05 14:40:24 -0400

    CH2 - Combinatorics (cubic) improved solution

--- ch_02/combinatorics_cubic.py
+++ ch_02/combinatorics_cubic.py
@@ -1,11 +1,16 @@
-# brute force
+# reduce from O(n^4) to O(n^3)
+
+import math
 
 def solve_cubic():
     for a in range(1, 100):
         for b in range(1, 100):
             for c in range(1, 100):
-                for d in range(1, 100):
-                    if a*a + b*b == c*c + d*d:
+                value = a * a + b * b - c * c
+
+                if value > 0:
+                    d = int(math.sqrt(value))
+                    if d < 100 and a * a + b * b == c * c + d * d:
                         print(f"{a}^2 + {b}^2 = {c}^2 + {d}^2")
 
 
