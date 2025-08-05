d76ab5607541348a9b60fdf5e14b4f6186e22f0f
Tom Zhang <tommyawesome08@gmail.com>
2025-08-05 11:02:29 -0400

    CH2 - Even or Odd Number

---
+++ ch_01/even_or_odd.py
@@ -0,0 +1,12 @@
+def is_even(num):
+    return num % 2 == 0
+
+
+def is_odd(num):
+    return num % 2 == 1
+
+
+print(is_even(101)) # False
+print(is_even(100)) # True
+print(is_odd(101)) # True
+print(is_odd(100)) # False
